d5c41fdb4a982d6d6ab31c3c8c9a7a68e2714091
psamll <48244353+psamll@users.noreply.github.com>
2024-09-23 09:43:01 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,8 @@
 
 // 代理
 let PROXYS: Proxy[] = [
-    {
+     {
         domain: "google-sand-rho.vercel.app",
-        url: "www.youtube.com/",
-        enable: true
-    },
-    {
-        domain: "www.example2.com",
         url: "www.google.com/",
         enable: true
     }
