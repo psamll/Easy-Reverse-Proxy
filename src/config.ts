36f412239d1496ab8296364bfbad89e74bf9d474
psamll <48244353+psamll@users.noreply.github.com>
2024-09-23 09:39:16 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,3 @@
-export let PORT = 370; // 端口
-
-export interface Proxy {
-    domain: string;
-    url: string;
-    enable: boolean;
-    replaces?: {
-        old: string,
-        new: string
-    }[];
-}
 
 // 代理
 let PROXYS: Proxy[] = [
